0d16bbc014e362877472b696dae37056631d43cb
nayanKhadase <khadsenayan1996@gmail.com>
2021-11-25 12:04:35 +0530

    Initial Commit

---
+++ collectionView In Table view cell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionView In Table view cell
+//
+//  Created by nayan.khadase on 25/11/21.
+//  Copyright © 2021 nayan.khadase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
